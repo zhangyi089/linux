f83833bffd12b58c7b0883b243628bd7c88a8a1d
Zhang Yi <yi.zhang@huawei.com>
2024-04-10 22:29:29 +0800

    ext4: update delalloc data reserve spcae in ext4_es_insert_extent()
    Now we update data reserved space for delalloc after allocating new
    blocks in ext4_{ind|ext}_map_blocks(). If bigalloc feature is enabled,
    we also need to query the extents_status tree and calculate the exact
    reserved clusters. This is complicated and it appears
    ext4_es_insert_extent() is a better place to do this job, it could make
    things simple because __es_remove_extent() could count delalloc blocks
    and __revise_pending() and return newly added pending count.
    
    One special case needs to concern is the quota claiming, when bigalloc
    is enabled, if the delayed cluster allocation has been raced by another
    no-delayed allocation which doesn't overlap the delayed blocks (from
    fallocate, filemap, DIO...) , we cannot claim quota as usual because the
    racer have already done it, so we also need to check the counted
    reserved blocks.
    
      |               one cluster               |
      -------------------------------------------
      |                            | delayed es |
      -------------------------------------------
      ^           ^
      | fallocate | <- don't claim quota
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/extents_status.c
+++ fs/ext4/extents_status.c
@@ -856,6 +856,8 @@ void ext4_es_insert_extent(struct inode *inode, ext4_lblk_t lblk,
 	struct extent_status newes;
 	ext4_lblk_t end = lblk + len - 1;
 	int err1 = 0, err2 = 0, err3 = 0;
+	struct rsvd_info rinfo;
+	int resv_used, pending = 0;
 	struct ext4_sb_info *sbi = EXT4_SB(inode->i_sb);
 	struct extent_status *es1 = NULL;
 	struct extent_status *es2 = NULL;
@@ -894,7 +896,7 @@ retry:
 		pr = __alloc_pending(true);
 	write_lock(&EXT4_I(inode)->i_es_lock);
 
-	err1 = __es_remove_extent(inode, lblk, end, NULL, es1);
+	err1 = __es_remove_extent(inode, lblk, end, &rinfo, es1);
 	if (err1 != 0)
 		goto error;
 	/* Free preallocated extent if it didn't get used. */
@@ -924,9 +926,27 @@ retry:
 			__free_pending(pr);
 			pr = NULL;
 		}
+		pending = err3;
 	}
 error:
 	write_unlock(&EXT4_I(inode)->i_es_lock);
+	/*
+	 * Reduce the reserved cluster count to reflect successful deferred
+	 * allocation of delayed allocated clusters or direct allocation of
+	 * clusters discovered to be delayed allocated.  Once allocated, a
+	 * cluster is not included in the reserved count.
+	 *
+	 * When bigalloc is enabled, allocating non-delayed allocated blocks
+	 * which belong to delayed allocated clusters (from fallocate, filemap,
+	 * DIO, or clusters allocated when delalloc has been disabled by
+	 * ext4_nonda_switch()). Quota has been claimed by ext4_mb_new_blocks(),
+	 * so release the quota reservations made for any previously delayed
+	 * allocated clusters.
+	 */
+	resv_used = rinfo.delonly_cluster + pending;
+	if (resv_used)
+		ext4_da_update_reserve_space(inode, resv_used,
+					     rinfo.delonly_block);
 	if (err1 || err2 || err3 < 0)
 		goto retry;
 
