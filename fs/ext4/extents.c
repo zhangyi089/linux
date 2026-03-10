a693c23d712b4c8799acdcd05976228016f946bc
Zhang Yi <yi.zhang@huawei.com>
2026-03-10 09:40:58 +0800

    ext4: remove handle parameters from zero partial block functions
    Only journal data mode requires an active journal handle when zeroing
    partial blocks. Stop passing handle_t *handle to
    ext4_zero_partial_blocks() and related functions, and make
    ext4_block_journalled_zero_range() start a handle independently.
    
    This change has no practical impact now because all callers invoke these
    functions within the context of an active handle. It prepares for moving
    ext4_block_zero_eof() out of an active handle in the next patch, which
    is a prerequisite for converting block zero range operations to iomap
    infrastructure.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -4625,8 +4625,7 @@ retry:
 						      inode_get_ctime(inode));
 			if (epos > old_size) {
 				pagecache_isize_extended(inode, old_size, epos);
-				ext4_block_zero_eof(handle, inode, old_size,
-						    epos);
+				ext4_block_zero_eof(inode, old_size, epos);
 			}
 		}
 		ret2 = ext4_mark_inode_dirty(handle, inode);
@@ -4744,7 +4743,7 @@ static long ext4_zero_range(struct file *file, loff_t offset,
 	}
 
 	/* Zero out partial block at the edges of the range */
-	ret = ext4_zero_partial_blocks(handle, inode, offset, len);
+	ret = ext4_zero_partial_blocks(inode, offset, len);
 	if (ret)
 		goto out_handle;
 
