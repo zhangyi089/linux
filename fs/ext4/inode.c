0e07e1a97ee5def6c7353235adc068f5aac11b43
Zhang Yi <yi.zhang@huawei.com>
2026-04-22 10:10:35 +0800

    ext4: add block mapping tracepoints for iomap buffered I/O path
    Add tracepoints for iomap buffered read, write, partial block zeroing,
    and writeback operations to help debug the iomap buffered I/O path.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3961,6 +3961,8 @@ static int ext4_iomap_buffered_read_begin(struct inode *inode, loff_t offset,
 	if (ret < 0)
 		return ret;
 
+	trace_ext4_iomap_buffered_read_begin(inode, &map, offset, length,
+					     flags);
 	ext4_set_iomap(inode, iomap, &map, offset, length, flags);
 	return 0;
 }
@@ -4036,6 +4038,8 @@ retry:
 	if (ret < 0)
 		return ret;
 
+	trace_ext4_iomap_buffered_write_begin(inode, &map, offset, length,
+					      flags);
 	ext4_set_iomap(inode, iomap, &map, offset, length, flags);
 	return 0;
 }
@@ -4138,6 +4142,7 @@ static int ext4_iomap_zero_begin(struct inode *inode,
 			map.m_len = (offset >> blkbits) - map.m_lblk;
 	}
 
+	trace_ext4_iomap_zero_begin(inode, &map, offset, length, flags);
 	ext4_set_iomap(inode, iomap, &map, offset, length, flags);
 	iomap->flags |= iomap_flags;
 
@@ -4306,6 +4311,7 @@ retry:
 		return ret;
 	}
 out:
+	trace_ext4_iomap_map_writeback_range(inode, &map, offset, dirty_len, 0);
 	ext4_set_iomap(inode, &wpc->iomap, &map, offset, dirty_len, 0);
 	return 0;
 }
